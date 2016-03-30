11f44bbe972bb8a068120fb31de07ea9a5c5c8ca
Christopher Steiner <steinch3@zhaw.ch>
2016-03-30 11:34:01 +0200

    Added files for Serie 5

---
+++ loetsphi_steinch3_S5_Aufg1.m
@@ -0,0 +1,12 @@
+function [ ] = loetsphi_steinch3_S5_Aufg1(f, xmin, xmax, ymin, ymax, hx, hy)
+%Zeigt das Richtungsfeld der Funktion auf
+%sample call
+%f = @(x, y) x.^2.*y.^2
+%loetsphi_steinch3_S5_Aufg1(f, 0, 5, 0, 3, 0.1, 0.1)
+[X, Y] = meshgrid(xmin:hx:xmax, ymin:hy:ymax);
+
+Ydiff = f(X, Y);
+quiver(X, Y, ones(size(X, 1), size(X, 2)), Ydiff); 
+
+end
+
