77d80c8192f9214f305fa7d3f8f178ef7e72209e
loetsphi <loetsphi@students.zhaw.ch>
2016-06-22 10:29:00 +0200

    next

--- ableiten_funkt.m
+++ ableiten_funkt.m
@@ -2,10 +2,13 @@ function [ dx ] = loetsphi_steinch3_S1_Aufg3( x, y )
 %Erste Ableitung numerisch berechnen
 dx = 0; % Steigungen
 n = length(x);
+%Für ersten Wert wird die Vorwärtsdifferenz benutzt
 dx(1) = (y(2)-y(1))/(x(2)-x(1));
+%Für alle Werte dazwischen die zentrale Differenz
 for i = 2:n-1
     dx(i) = (y(i+1)-y(i-1))/(x(i+1)-x(i-1));
 end
+%Für den letzten Wert muss die Rückwärtsdifferenz benutzt werden
 dx(n) = (y(n)-y(n-1))/(x(n)-x(n-1)); 
 end
 
