ad61397123d55df170d3ed184442ff0380f0282f
Christopher Steiner <steinch3@zhaw.ch>
2016-06-22 14:11:47 +0200

    renamed function

--- loetsphi_steinch3_S4_Aufg1.m
+++ loetsphi_steinch3_S4_Aufg1.m
@@ -3,10 +3,10 @@ h = [2 286 1268 3009 5375 8220 11505 15407 20127 25593 31672 38257 44931];
 m = [2051113 1935155 1799290 1681120 1567611 1475282 1376301 1277921 1177704 1075683 991872 913254 880377];
 
 % a)
-v = loetsphi_steinch3_S1_Aufg3(t, h);
-a = loetsphi_steinch3_S1_Aufg3(t, v);
-dmdt = loetsphi_steinch3_S1_Aufg3(t, m);
-dmdh = loetsphi_steinch3_S1_Aufg3(h, m);
+v = ableiten_funkt(t, h);
+a = ableiten_funkt(t, v);
+dmdt = ableiten_funkt(t, m);
+dmdh = ableiten_funkt(h, m);
 
 plot(t, h, 'r');
 plot(t, v, 'r');
