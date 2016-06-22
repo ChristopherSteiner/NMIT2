a0d67ac65808ecb29931a620e1e859d56ec2dd66
loetsphi <loetsphi@students.zhaw.ch>
2016-06-22 14:11:52 +0200

    trapez äqui

--- loetsphi_steinch3_S4_Aufg1.m
+++ loetsphi_steinch3_S4_Aufg1.m
@@ -1,8 +1,9 @@
 t = 0:10:120;
 h = [2 286 1268 3009 5375 8220 11505 15407 20127 25593 31672 38257 44931];
 m = [2051113 1935155 1799290 1681120 1567611 1475282 1376301 1277921 1177704 1075683 991872 913254 880377];
-
-% a)
+%Space Shuttle Berechnung
+% a) Geschwindigkeit, Beschleuningung, Massenänderung(t) und Massenänderung(h) anhand Input Daten
+% berechnen
 v = loetsphi_steinch3_S1_Aufg3(t, h);
 a = loetsphi_steinch3_S1_Aufg3(t, v);
 dmdt = loetsphi_steinch3_S1_Aufg3(t, m);
